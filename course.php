849de4ac2ead0e24485d9391cc7247870549b75d
NeroZhangTianpeng <neroztp@gmail.com>
2017-10-05 10:02:28 +0200

    Update course.php

--- course.php
+++ course.php
@@ -1,37 +1,19 @@
 <p>Course System</p>
 <p> Register compulsory courses</p>
 <table>
-
-  <tr><th>Course Name</th>
-  </tr>
-
-  <tr><th>Pre-requisite Course</th>
-    <td>NUll</td>
-  </tr>
-
-  <td><th>Course Time</th>
-    <tr>Math 1</tr>
-  </td>
-
-  <td><th>Course Teacher</th>
-    <tr>Math 1</tr>
-  </td>
-
-  <td><th>Choose Or Not</th>
-    <tr>Math 1</tr>
-  </td>
-
-  <tr>NULL</tr>
   <tr>
-    <td>Course Time</td>
-    <td></td>
-  </tr>
+    <th>Course Name</th>
+    <th>Pre-requisite Course<th>
+    <th>Course Time<th>
+    <th>Course Teacher<th>
+    <th>Choose Or Not<th>
+   </tr>
+  
   <tr>
-    <td>Course Teacher</td>
-    <td></td>
-  </tr>
-  <tr>
-    <td>Choose or not</td>
-    <td><a href="chooseSuccess.php" target="_parent">Choose</a></td>
+    <td>Math2</td>
+    <td>Math1</td>
+    <td>2017</td>
+    <td>Zhang</td>
+    <td>Not</td>
   </tr>
 </table>
