11d5f7aae7a301a6bd2c57fae25f73956f788889
Carrin <chinacarrin112@outlook.com>
2017-10-05 09:13:29 +0200

    Upload all the documents

---
+++ includes/footer.php
@@ -0,0 +1,21 @@
+<!-- Footer -->
+<footer>
+    <div class="">
+
+        <div class="col-lg-12"><p class="text-center">Copyrights &copy 2017 Team 7 </p></div>
+    </div>
+    <!-- /.row -->
+</footer>
+
+</div>
+<!-- /.container -->
+
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
