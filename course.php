c52be5e2b9f5d3a58f4b2e9b5c32e71199a86137
Carrin <chinacarrin112@outlook.com>
2017-10-05 09:56:21 +0200

    Add course

---
+++ course.php
@@ -0,0 +1,37 @@
+<p>Course System</p>
+<p> Register compulsory courses</p>
+<table>
+
+  <tr><th>Course Name</th>
+  </tr>
+
+  <tr><th>Pre-requisite Course</th>
+    <td>NUll</td>
+  </tr>
+
+  <td><th>Course Time</th>
+    <tr>Math 1</tr>
+  </td>
+
+  <td><th>Course Teacher</th>
+    <tr>Math 1</tr>
+  </td>
+
+  <td><th>Choose Or Not</th>
+    <tr>Math 1</tr>
+  </td>
+
+  <tr>NULL</tr>
+  <tr>
+    <td>Course Time</td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>Course Teacher</td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>Choose or not</td>
+    <td><a href="chooseSuccess.php" target="_parent">Choose</a></td>
+  </tr>
+</table>
--- login.php
+++ login.php
@@ -1,3 +1,7 @@
-<?php
-echo "successfully";
-?>
+
+<p>Course System</p>
+<form action="welcome.php" method="post">
+ID: <input type="text" name="fname">
+Password: <input type="text" name="age">
+<input type="submit" value="提交">
+</form>
