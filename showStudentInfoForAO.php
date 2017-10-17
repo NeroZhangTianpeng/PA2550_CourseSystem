25e4812943fc3504406340471eccdbb9244a6065
RLX <ruanlx22@gmail.com>
2017-10-17 16:18:08 +0200

    change the style of table

--- showStudentInfoForAO.php
+++ showStudentInfoForAO.php
@@ -39,24 +39,38 @@
             
                 $result = $conn->query($sql);
 				while ($row = $result->fetch_assoc()) {
-					echo "<tr><td>StudentID: </td><td>" .$row['studentId']. "</td>
-							<td>StudentName: </td><td>" .$row['studentName']. "</td></tr>";
+					echo "<p>StudentID: " .$row['studentId']."	StudentName: " .$row['studentName']. "</p>";
 				}
 
 				$result2 = $conn->query($sql2);
-				while ($row2 = $result2->fetch_assoc()) {
-                    echo "<tr><td>CourseID: </td><td>" .$row['courseId']. 
-                    "</td><td>CourseMark: </td><td>" .$row['mark']. 
-                    "</td><td>CourseName: </td><td>" .$row['courseName'].
-                    "</td><td>CourseState: </td><td>" .$row['courseState'].
-                    "</td><td>CourseFee: </td><td>" .$row['courseFee'].
-                    "</td><td>CourseTeacher: </td><td>" .$row['courseTeacher'].
-					"</td><td>TimeOfExam: </td><td>".$row['timeOfExam'].
-                    "</td><td>CourseCredit: </td><td>" .$row['courseCredit'].
-                    "</td><td>Pre-requisiteCourse: </td><td>" .$row['Pre-requisiteCourse'].
+				echo "<thead>
+					<tr>
+						<td>CourseID</td>
+						<td>CourseName</td>
+						<td>CourseState</td>
+						<td>CourseFee</td>
+						<td>CourseTeacher</td>
+						<td>CourseCredit</td>
+						<td>Pre-requisiteCourse</td>
+						<td>TimeOfExam</td>
+						<td>CourseMark</td>
+					</tr>
+				</thead>
+				<tbody>";
+				while ($row = $result2->fetch_assoc()) {
+                    echo "<tr><td>" .$row['courseId']. 
+                    "</td><td>" .$row['courseName']. 
+                    "</td><td>" .$row['courseState'].
+                    "</td><td>" .$row['courseFee'].
+                    "</td><td>" .$row['courseTeacher'].
+                    "</td><td>" .$row['courseCredit'].
+					"</td><td>".$row['Pre-requisiteCourse'].
+                    "</td><td>" .$row['timeOfExam'].
+                    "</td><td>" .$row['mark'].
                     "</td></tr>";
                     	
                 }
+				echo "</tbody>";
       
     	}
 
